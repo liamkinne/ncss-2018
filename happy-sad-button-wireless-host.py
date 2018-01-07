709d1f2a5295d90677f4c1d12e53b83e10400625
Liam Kinne <liamkinne@users.noreply.github.com>
2018-01-07 17:31:17 +1100

    comment and document

--- happy-sad-button-wireless-host.py
+++ happy-sad-button-wireless-host.py
@@ -1,6 +1,7 @@
 from microbit import *
 import radio
 
+# start radio
 radio.on()
 radio.config(channel=69)
 
@@ -8,10 +9,10 @@ state = False
 pin_last = False
 
 while True:
-    pin_state = pin0.read_digital()
-    if (pin_state) and (not pin_state == pin_last):
+    pin_state = pin0.read_digital() # read state 
+    if (pin_state) and (not pin_state == pin_last): # is on and changed (rising edge)
         state = not state
         radio.send(str(state))
-        print(state)
-    
+        #print(state) # debug
+    # remember state
     pin_last = pin_state
