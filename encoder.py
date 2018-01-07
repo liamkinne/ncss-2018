a795e3a9a81a812e812047d2fd7552ca9ad2fcd4
Liam Kinne <liamkinne@users.noreply.github.com>
2018-01-07 17:28:01 +1100

    comment and document

--- encoder.py
+++ encoder.py
@@ -2,33 +2,33 @@ from microbit import *
 
 pin_last_a = False
 pin_last_b = False
-
 counter = 0
 
+# set pullups
 pin0.read_digital()
 pin0.set_pull(pin0.PULL_UP)
 pin13.read_digital()
 pin13.set_pull(pin13.PULL_UP)
 
 while True:
-    pin_state_a = pin0.read_digital() # Get state
-    pin_state_b = pin13.read_digital() # Get state
+    # get states
+    pin_state_a = pin0.read_digital()
+    pin_state_b = pin13.read_digital()
     
-    if pin_state_b and pin_state_b != pin_last_b:
-        if (not pin_state_a and pin_state_b):
+    # clockwise
+    if pin_state_b and pin_state_b != pin_last_b: # check is on and rising edge
+        if (not pin_state_a and pin_state_b):     # check if before other pin
             counter += 1
-            print("up")
+            #print("up") # debug
     
-    if pin_state_a and pin_state_a != pin_last_a:
-        if (not pin_state_b and pin_state_a):
+    # anti-clockwise
+    if pin_state_a and pin_state_a != pin_last_a: # check is on and rising edge
+        if (not pin_state_b and pin_state_a):     # check if before other pin
             counter -= 1
-            print("down")
+            #print("down") # debug
     
+    display.scroll(str(counter), wait=False);     # display
     
-    #Old
-    #if pin_state_a and not pin_last_b:
-    #    counter += 1 # incriment
-    #    print(counter)
-    
+    # remember states
     pin_last_a = pin_state_a
     pin_last_b = pin_state_b
