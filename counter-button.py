42083e04a29fffe55eae3c759a2b0dff7b868f63
Liam Kinne <liamkinne@users.noreply.github.com>
2018-01-07 17:22:09 +1100

    Added comments

--- counter-button.py
+++ counter-button.py
@@ -1,13 +1,14 @@
+# Incriment a number when a button is pressed
+# then print it.
+
 from microbit import *
 
 pin_last = False
-
 counter = 0
 
 while True:
     pin_state = pin0.read_digital() # Get state
-    if pin_state and not pin_last:
-        counter += 1 # incriment
-        print(counter)
-    
-    pin_last = pin_state
+    if pin_state and not pin_last:  # check for pin change
+        counter += 1                # incriment
+        print(counter)              # print
+    pin_last = pin_state            # remember last state
