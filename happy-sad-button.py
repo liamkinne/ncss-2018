dbedbadbf3b2e63c39cf8b68c5075fb87e358ab9
Liam Kinne <liamkinne@users.noreply.github.com>
2018-01-07 17:34:00 +1100

    comment and document

--- happy-sad-button.py
+++ happy-sad-button.py
@@ -1,18 +1,12 @@
 from microbit import *
 
 state = False
-
 pin_last = False
 
-faces = {
-    True : Image.HAPPY,
-    False: Image.SAD,
-}
-
 while True:
-    pin_state = pin0.read_digital()
-    if (pin_state) and (not pin_state == pin_last):
-        state = not state
-        display.show(faces[state])
-    
+    pin_state = pin0.read_digital()                         # read pin
+    if (pin_state) and (not pin_state == pin_last):         # high and changing state (rising edge)
+        state = not state                                   # invert state
+        display.show(Image.HAPPY if state else Image.SAD)   # show image
+    # remember state
     pin_last = pin_state
