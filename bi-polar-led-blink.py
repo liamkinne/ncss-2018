40b19a9aa8b32b5a6e497aff6b8b74b53d9da004
liamkinne <kinneliam@gmail.com>
2018-01-08 11:27:21 +1100

    added lab 1.4

---
+++ bi-polar-led-blink.py
@@ -0,0 +1,13 @@
+# bi-polar led blink
+from microbit import *
+
+state = False
+time_last = running_time()
+delay = 500 #time between state changes
+
+while True:
+    if (running_time() - time_last) >= delay:
+        pin13.write_digital(state)  # write output
+        pin14.write_digital(not state)  # write output
+        state = not state           # invert state
+        time_last = running_time()  # store time
