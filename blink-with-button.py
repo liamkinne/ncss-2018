c911075ea55399063ce0a8f7ee01cc50621d08b6
liamkinne <kinneliam@gmail.com>
2018-01-08 11:35:38 +1100

    use internal pulldown

--- blink-with-button.py
+++ blink-with-button.py
@@ -5,7 +5,7 @@ from microbit import *
 
 # set as input
 pin14.read_digital()
-pin14.set_pull(pin5.NO_PULL)
+pin14.set_pull(pin5.PULL_DOWN)
 
 while True:
     pin13.write_digital(pin14.read_digital())
