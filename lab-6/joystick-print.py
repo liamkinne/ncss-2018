fa5fc5db6698196021831c5b1e1ef0ee02add699
liamkinne <kinneliam@gmail.com>
2018-01-08 16:58:34 +1100

    put values in list

--- lab-6/joystick-print.py
+++ lab-6/joystick-print.py
@@ -3,9 +3,13 @@ from microbit import *
 
 # turn off display
 display.off()
+
 # better naming
 pin_x = pin3
 pin_y = pin4
+
+# value storage
+values = [0, 0]
 
 # set as inputs
 pin_x.read_analog()
@@ -14,7 +18,6 @@ pin_y.read_analog()
 while True:
     x_in = pin_x.read_analog() / 1024.0
     y_in = pin_y.read_analog() / 1024.0
-    x_in -= 0.5
-    y_in -= 0.5
+    [(x/1024.0)-1.0 for x in values]
     
-    print("X" + str(x_in)[] + ", Y" + str(y_in))
+    print("X" + str(x_in)[:4] + ", Y" + str(y_in)[:4])
