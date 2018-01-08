2e31e44fd611bc4190e268414ade3f0be44321d4
liamkinne <kinneliam@gmail.com>
2018-01-08 11:34:12 +1100

    added lab 2.1

---
+++ blink-with-button.py
@@ -0,0 +1,11 @@
+# blink from button
+# led on pin 13
+# button on pin 14
+from microbit import *
+
+# set as input
+pin14.read_digital()
+pin14.set_pull(pin5.NO_PULL)
+
+while True:
+    pin13.write_digital(pin14.read_digital())
